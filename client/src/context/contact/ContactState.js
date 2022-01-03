a777f6fb903560867472238fcd2e2afd0a1dfa60
Florent VICENTE <vicentegroupe@gmail.com>
2022-01-03 19:16:17 +0100

    contacts api integration & deploy on heroku; Still some axios issue on the front end, have to check

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -2,16 +2,36 @@ import React, { useReducer } from 'react'
 import axios from 'axios'
 import { ContactContext } from './contactContext'
 import { contactReducer } from './contactReducer'
-import { ADD_CONTACT, DELETE_CONTACT, SET_CURRENT, CLEAR_CURRENT, UPDATE_CONTACT, FILTER_CONTACT, CLEAR_FILTER, CONTACT_ERROR } from '../types'
+import { ADD_CONTACT, DELETE_CONTACT, SET_CURRENT, CLEAR_CURRENT, UPDATE_CONTACT, FILTER_CONTACT, CLEAR_FILTER, CONTACT_ERROR, GET_CONTACTS, CLEAR_CONTACTS } from '../types'
 
 export const ContactState = props => {
   const initialState = {
-    contacts: [],
+    contacts: null,
     current: null,
-    filtered: null
+    filtered: null,
+    error: null,
   }
 
   const [ state, dispatch ] = useReducer(contactReducer, initialState)
+
+  const getContacts = async () => {
+
+    try {
+      const res = await axios.get("/api/contacts")
+      dispatch({
+        type: GET_CONTACTS,
+        payload: res.data
+      })
+
+    } catch (error) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: error.response.msg
+      })
+    }
+
+  }
+
 
   const addContact = async contact => {
     const config = {
@@ -36,8 +56,49 @@ export const ContactState = props => {
 
   }
 
-  const deleteContact = id => {
-    dispatch({ type: DELETE_CONTACT, payload: id })
+  const deleteContact = async id => {
+    try {
+      await axios.delete(`/api/contacts/${id}`)
+
+      dispatch({
+        type: DELETE_CONTACT,
+        payload: id
+      })
+
+    } catch (error) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: error.response.msg
+      })
+    }
+  }
+
+
+  const updateContact = async contact => {
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+
+    try {
+      const res = await axios.put(`/api/contacts/${contact._id}`, contact, config)
+      dispatch({
+        type: UPDATE_CONTACT,
+        payload: res.data
+      })
+
+    } catch (error) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: error.response.msg
+      })
+    }
+  }
+
+  const clearContacts = () => {
+    dispatch({ type: CLEAR_CONTACTS })
   }
 
   const setCurrent = contact => {
@@ -46,10 +107,6 @@ export const ContactState = props => {
 
   const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT })
-  }
-
-  const updateContact = contact => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact })
   }
 
   const filterContact = text => {
@@ -66,13 +123,16 @@ export const ContactState = props => {
         contacts: state.contacts,
         current: state.current,
         filtered: state.filtered,
+        error: state.error,
         addContact,
         deleteContact,
         setCurrent,
         clearCurrent,
         updateContact,
         filterContact,
-        clearFilter
+        clearFilter,
+        getContacts,
+        clearContacts
       }}>
       {props.children}
     </ContactContext.Provider>
